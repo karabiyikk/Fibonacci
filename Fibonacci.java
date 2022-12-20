fd2e5c9414f339eea34c7dbd5b57553cbc819d31
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-20 12:24:19 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int i, a = 0, b = 1, toplam = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç adet Fibonacci Sayısı görmek istiyorsunuz : ");
+        int n = input.nextInt();
+        System.out.print("Fibonacci Sayıları : ");
+
+        for (i = 0; i <= n - 1; i++) {
+            a = b;
+            b = toplam;
+            toplam = a + b;
+            System.out.print(toplam + ",");
+        }
+    }
+}
